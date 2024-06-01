940108dfcc2daffec7e66d67f09a3d312ecf4024
Misael <santiagoalamos2000@gmail.com>
2024-06-01 13:59:22 -0300

    m

--- src/index.js
+++ src/index.js
@@ -10,7 +10,8 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <RouterProvider router={router} /> */} <App/>
+    {/* <RouterProvider router={router} /> */} 
+    {/* <App/> */} App
   </React.StrictMode>
 );
 
