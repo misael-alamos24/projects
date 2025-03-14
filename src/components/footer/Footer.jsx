d7fceb45ddbe80c3c69e69918152b5b34f09d810
Santiago Alamos <166983296+misael-alamos24@users.noreply.github.com>
2025-03-13 22:14:34 -0300

    march2025

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,6 +4,7 @@ export default function Footer () {
     return (
         <div className="h10 footer">
             © 2024 Desarrollo Web <span style={{fontFamily: 'serif'}}>Stack</span>
+            Goat
         </div>
     )
 }
